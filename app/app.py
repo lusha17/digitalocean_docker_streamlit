68166ea5246b6f152bdaa226055f058a2bfb02e3
lusha17 <antonlushkin2307@yandex.ru>
2022-07-24 20:11:16 +0300

    add

--- app/app.py
+++ app/app.py
@@ -1,280 +1,130 @@
-from typing import List
-
-import cv2
-import torch
-import numpy as np
-import pandas as pd
-import streamlit as st
-import matplotlib.colors as mcolors
-from PIL import Image
-from streamlit_webrtc import VideoProcessorBase, webrtc_streamer, WebRtcMode
-
-from config import CLASSES, WEBRTC_CLIENT_SETTINGS
-
-import threading
-
-import cv2
-from matplotlib import pyplot as plt
-
-lock = threading.Lock()
-img_container = {"img": None}
-
+import queue
+import urllib.request
+from pathlib import Path
+from typing import List, NamedTuple, Optional
 
 import av
+import cv2
+import numpy as np
+import streamlit as st
+from streamlit_webrtc import (RTCConfiguration,WebRtcMode,WebRtcStreamerContext,webrtc_streamer,)
 
-def video_frame_callback(frame):
-    img = frame.to_ndarray(format="bgr24")
-    #with lock:
-    #    img_container["img"] = img
-    return frame
-
-#def video_frame_callback(frame):
-#    img = frame.to_ndarray(format="bgr24")
-#    flipped = img[::-1,:,:] if flip else img
-#    return av.VideoFrame.from_ndarray(flipped, format="bgr24")
-
-#изменим название страницы, отображаемое на вкладке браузера
-#set_page_config должна вызываться до всех функций streamlit
-st.set_page_config(
-    page_title="Weapon Detection Demo",
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
 )
 
-st.title('Weapon Detection Demo')
+CLASSES = [ 'bill', 'card', 'face', 'knife', 'mask', 'firearm', 'purse', 'smartphone']
 
-#region Functions
-# --------------------------------------------
+@st.experimental_singleton
+def generate_label_colors():
+    return np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
-@st.cache(max_entries=2)
-def get_yolo5():
-    '''
-    Возвращает модель YOLOv5 из Torch Hub типа `model_type`
+def detect(image, net):
+    blob = cv2.dnn.blobFromImage(image, 1/255.0, (640, 640), swapRB=True, crop=False)
+    net.setInput(blob)
+    preds = net.forward()
+    return preds
 
-    Arguments
-    ----------
-    model_type : str, 's', 'm', 'l' or 'x'
-        тип модели - s - самая быстрая и неточная, x - самая точная и медленная
+def wrap_detection(input_image, output_data):
+    class_ids = []
+    confidences = []
+    boxes = []
+    rows = output_data.shape[0]
+    image_width, image_height, _ = input_image.shape
+    x_factor = image_width / 640
+    y_factor =  image_height / 640
+    for r in range(rows):
+        row = output_data[r]
+        confidence = row[4]
+        if confidence >= 0.4:
+            classes_scores = row[5:]
+            _, _, _, max_indx = cv2.minMaxLoc(classes_scores)
+            class_id = max_indx[1]
+            if (classes_scores[class_id] > .25):
+                confidences.append(confidence)
+                class_ids.append(class_id)
+                x, y, w, h = row[0].item(), row[1].item(), row[2].item(), row[3].item() 
+                left = int((x - 0.5 * w) * x_factor)
+                top = int((y - 0.5 * h) * y_factor)
+                width = int(w * x_factor)
+                height = int(h * y_factor)
+                box = np.array([left, top, width, height])
+                boxes.append(box)
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.25, 0.45) 
+    result_class_ids = []
+    result_confidences = []
+    result_boxes = []
+    for i in indexes:
+        result_confidences.append(confidences[i])
+        result_class_ids.append(class_ids[i])
+        result_boxes.append(boxes[i])
+    return result_class_ids, result_confidences, result_boxes
 
-    Returns
-    -------
-    torch model
-        torch-модель типа `<class 'models.common.autoShape'>`
-    '''
-    #return torch.hub.load('ultralytics/yolov5', 'yolov5{}'.format(model_type), pretrained=True)
-    #return torch.hub.load('ultralytics/yolov5', 'custom', path='last.pt', force_reload=True)  
-    return torch.hub.load('ultralytics/yolov5', 'custom', path='all.pt', force_reload=True)
+def format_yolov5(frame):
+    row, col, _ = frame.shape
+    _max = max(col, row)
+    result = np.zeros((_max, _max, 3), np.uint8)
+    result[0:row, 0:col] = frame
+    return result
 
-@st.cache(max_entries=10)
-def get_preds(img : np.ndarray) -> np.ndarray:
-    """
-    Возвращает прогнозы, полученные от YOLOv5
+colors = generate_label_colors()
 
-    Arguments
-    ---------
-    img : np.ndarray
-        RGB-изображение загруженное с помощью OpenCV
+DEFAULT_CONFIDENCE_THRESHOLD = 0.5
 
-    Returns
-    -------
-    2d np.ndarray
-        Список найденных объектов в формате 
-        `[xmin,ymin,xmax,ymax,conf,label]`
-    """
-    return model([img]).xyxy[0].numpy()
 
-def get_colors(indexes : List[int]) -> dict:
-    '''
-    Возвращает цвета для всех выбранных классов. Цвета формируются 
-    на основе наборов TABLEAU_COLORS и BASE_COLORS из Matplotlib
-
-    Arguments
-    ----------
-    indexes : list of int
-        список индексов классов в порядке по умолчанию для 
-        MS COCO (80 классов, без background)
-
-    Returns
-    -------
-    dict
-        словарь, в котором ключи - id классов, указанные в 
-        indexes, а значения - tuple с компонентами rgb цвета, например, (0,0,0)
-    '''
-    to_255 = lambda c: int(c*255)
-    tab_colors = list(mcolors.TABLEAU_COLORS.values())
-    tab_colors = [list(map(to_255, mcolors.to_rgb(name_color))) 
-                                                for name_color in tab_colors]
-    base_colors = list(mcolors.BASE_COLORS.values())
-    base_colors = [list(map(to_255, name_color)) for name_color in base_colors]
-    rgb_colors = tab_colors + base_colors
-    rgb_colors = rgb_colors*5
-
-    color_dict = {}
-    for i, index in enumerate(indexes):
-        if i < len(rgb_colors):
-            color_dict[index] = rgb_colors[i]
-        else:
-            color_dict[index] = (255,0,0)
-
-    return color_dict
-
-def get_legend_color(class_name : int):
-    """
-    Возвращает цвет ячейки для `pandas.Styler` при создании легенды. 
-    Раскарасит ячейку те же цветом, который имеют боксы соотвествующего класс
-
-    Arguments
-    ---------
-    class_name : int
-        название класса согласно списку классов MS COCO
-
-    Returns
-    -------
-    str
-        background-color для ячейки, содержащей class_name
-    """  
-
-    index = CLASSES.index(class_name)
-    color = rgb_colors[index]
-    return 'background-color: rgb({color[0]},{color[1]},{color[2]})'.format(color=color)
-
-class VideoTransformer(VideoProcessorBase):
-    """Компонент для создания стрима веб камеры"""
-    def __init__(self):
-        self.model = model
-        self.rgb_colors = rgb_colors
-        self.target_class_ids = target_class_ids
-
-    def get_preds(self, img : np.ndarray) -> np.ndarray:
-        return self.model([img]).xyxy[0].numpy()
-
-    def transform(self, frame):
-        img = frame.to_ndarray(format="bgr24")
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-        result = self.get_preds(img)
-        result = result[np.isin(result[:,-1], self.target_class_ids)]
+class Detection(NamedTuple):
+    name: str
+    prob: float
         
-        for bbox_data in result:
-            xmin, ymin, xmax, ymax, _, label = bbox_data
-            p0, p1, label = (int(xmin), int(ymin)), (int(xmax), int(ymax)), int(label)
-            img = cv2.rectangle(img, 
-                                    p0, p1, 
-                                    self.rgb_colors[label], 2) 
+cache_key = "object_detection_dnn"
 
-        return cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-
-#endregion
-
-
-#region Load model
-# ---------------------------------------------------
-
-#model_type = st.sidebar.selectbox('Select model type',('s', 'm', 'l', 'x'),index=1,format_func=lambda s: s.upper())
-
-with st.spinner('Loading the model...'):
-    model = get_yolo5()
-st.success('Loading the model.. Done!')
-#endregion
-
-
-# UI elements
-# ----------------------------------------------------
-
-#sidebar
-prediction_mode = st.sidebar.radio(
-    "",
-    ('Single image', 'Web camera'),
-    index=0)
-    
-#classes_selector = st.sidebar.multiselect('Select classes', CLASSES, default='pistol')
-classes_selector = st.sidebar.multiselect('Select classes', CLASSES, default='pistol')
-all_labels_chbox = st.sidebar.checkbox('All classes', value=False)
-
-
-# Prediction section
-# ---------------------------------------------------------
-
-#target labels and their colors
-#target_class_ids - индексы выбранных классов согласно списку классов MS COCC
-#rgb_colors - rgb-цвета для выбранных классов
-if all_labels_chbox:
-    target_class_ids = list(range(len(CLASSES)))
-elif classes_selector:
-    target_class_ids = [CLASSES.index(class_name) for class_name in classes_selector]
+if cache_key in st.session_state:
+    net = st.session_state[cache_key]
 else:
-    target_class_ids = [0]
+    net = cv2.dnn.readNetFromONNX('all_s.onnx')
+    st.session_state[cache_key] = net
 
-rgb_colors = get_colors(target_class_ids)
-detected_ids = None
+#confidence_threshold = st.slider("Confidence threshold", 0.0, 1.0, DEFAULT_CONFIDENCE_THRESHOLD, 0.05)
 
+def _annotate_image(image, detections):
+    inputImage = format_yolov5(image)
+    result: List[Detection] = []
+    for i in np.arange(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > confidence_threshold:
+            idx = int(detections[0, 0, i, 1])
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+            name = CLASSES[idx]
+            result.append(Detection(name=name, prob=float(confidence)))
+            label = f"{name}: {round(confidence * 100, 2)}%"
+            cv2.rectangle(image, (startX, startY), (endX, endY), COLORS[idx], 2)
+            y = startY - 15 if startY - 15 > 15 else startY + 15
+            cv2.putText(image,label,(startX, y),cv2.FONT_HERSHEY_SIMPLEX,0.5,COLORS[idx],2,)
+    return image, result
 
-if prediction_mode == 'Single image':
+result_queue = (queue.Queue())
 
-    # добавляет форму для загрузки изображения
-    uploaded_file = st.file_uploader(
-        "Choose an image",
-        type=['png', 'jpg', 'jpeg'])
+def callback(frame):
+    image = frame.to_ndarray(format="bgr24")
+    inputImage = format_yolov5(image)
+    outs = detect(inputImage, net)
+    class_ids, confidences, boxes = wrap_detection(inputImage, outs[0])
+    for (classid, confidence, box) in zip(class_ids, confidences, boxes):
+        color = colors[int(classid) % len(colors)]
+        image = cv2.rectangle(image, box, color, 2)
+        image = cv2.rectangle(image, (box[0], box[1] - 20), (box[0] + box[2], box[1]), color, -1)
+        image = cv2.putText(image, CLASSES[classid], (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, .5, (0,0,0))
+    #result_queue.put(result)  
+    return av.VideoFrame.from_ndarray(image, format="bgr24")
 
-    # если файл загружен
-    if uploaded_file is not None:
-
-        # конвертация изображения из bytes в np.ndarray
-        bytes_data = uploaded_file.getvalue()
-        file_bytes = np.asarray(bytearray(bytes_data), dtype=np.uint8)
-        img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        result = get_preds(img)
-
-        #скопируем результаты работы кэшируемой функции, чтобы не изменить кэш
-        result_copy = result.copy()
-        #отберем только объекты нужных классов
-        result_copy = result_copy[np.isin(result_copy[:,-1], target_class_ids)]
-        
-
-        detected_ids = []
-        #также скопируем изображение, чтобы не изменить аргумент кэшируемой 
-        # функции get_preds
-        img_draw = img.copy().astype(np.uint8)
-        # нарисуем боксы для всех найденных целевых объектов
-        for bbox_data in result_copy:
-            xmin, ymin, xmax, ymax, _, label = bbox_data
-            p0, p1, label = (int(xmin), int(ymin)), (int(xmax), int(ymax)), int(label)
-            img_draw = cv2.rectangle(img_draw, 
-                                    p0, p1, 
-                                    rgb_colors[label], 2) 
-            detected_ids.append(label)
-        
-        # выведем изображение с нарисованными боксами
-        # use_column_width растянет изображение по ширине центральной колонки
-        st.image(img_draw, use_column_width=True)
-
-elif prediction_mode == 'Web camera':
-    # создаем объект для вывода стрима с камеры
-    #ctx = webrtc_streamer(
-    #    key="example", 
-    #    rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
-    #    media_stream_constraints={"video": True, "audio": True},
-    #    video_frame_callback=video_frame_callback
-    #)
-    ctx = webrtc_streamer(
-        key="example", 
-        rtc_configuration={"iceServers": [{"urls": ["stun:stun.xten.com:3478"]}]},
-        mode=WebRtcMode.SENDRECV,
-        media_stream_constraints={"video": True, "audio": True},
-        video_frame_callback=video_frame_callback,
-        async_processing=True
-    )
-    #video_processor_factory=VideoTransformer,
-    #video_frame_callback=video_frame_callback
-    # необходимо для того, чтобы объект VideoTransformer подхватил новые данные
-    # после обновления страницы streamlit
-    #if ctx.video_transformer:
-    #    ctx.video_transformer.model = model
-    #    ctx.video_transformer.rgb_colors = rgb_colors
-    #    ctx.video_transformer.target_class_ids = target_class_ids
-# выведем список найденных классов при работе с изображением или список всех
-# выбранных классов при работе с видео
-detected_ids = set(detected_ids if detected_ids is not None else target_class_ids)
-labels = [CLASSES[index] for index in detected_ids]
-legend_df = pd.DataFrame({'label': labels})
-st.dataframe(legend_df.style.applymap(get_legend_color))
+webrtc_ctx = webrtc_streamer(key="object-detection",mode=WebRtcMode.SENDRECV,rtc_configuration=RTC_CONFIGURATION,video_frame_callback=callback,media_stream_constraints={"video": True, "audio": False},async_processing=True,)
+if st.checkbox("Show the detected labels", value=False):
+    if webrtc_ctx.state.playing:
+        labels_placeholder = st.empty()
+        while True:
+            try:
+                result = result_queue.get(timeout=1.0)
+            except queue.Empty:
+                result = None
+            labels_placeholder.table(result)
